3d6a7a63d5c4848522d7287bc99b3f0e1b3ce510
huifer <wangtaolee97@gmail.com>
2019-03-03 20:38:44 +0800

    spring - 注解

---
+++ myspringBook/src/main/java/com/huifer/comment/UserService.java
@@ -0,0 +1,16 @@
+package com.huifer.comment;
+
+
+/**
+ * 描述:
+ *
+ * @author huifer
+ * @date 2019-03-03
+ */
+public interface UserService {
+
+    /**
+     * 保存用户
+     */
+    void saveUser();
+}
---
+++ myspringBook/src/main/java/com/huifer/comment/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.huifer.comment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * 描述:
+ *
+ * @author huifer
+ * @date 2019-03-03
+ */
+@Component(value = "userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private BeanT bt;
+
+
+    @Resource(name = "bt")
+    private BeanT btt;
+
+    @Value(value = "1222")
+    private int dc;
+    @Value(value = "${data.pc}")
+    private String pc;
+    @Override
+    public void saveUser() {
+        System.out.println(bt);
+        System.out.println(btt.i);
+        System.out.println(dc);
+        System.out.println(pc);
+        System.out.println("注解配置");
+    }
+
+}
