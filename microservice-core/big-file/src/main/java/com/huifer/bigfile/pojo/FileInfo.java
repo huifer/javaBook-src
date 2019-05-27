43fada5aa54c99462c46f785164f87a669210720
huifer <1187982580@qq.com>
2019-05-27 15:51:41 +0800

    大文件上传

---
+++ microservice-core/big-file/src/main/java/com/huifer/bigfile/pojo/FileInfo.java
@@ -0,0 +1,51 @@
+package com.huifer.bigfile.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>Title : FileInfo </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-05-27
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FileInfo implements Serializable {
+
+    private static final long serialVersionUID = -1428891800897624020L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "md5")
+    private String md5;
+    @Column(name = "path")
+    private String path;
+    @Column(name = "uploadTime")
+    private Date uploadTime;
+    @Column(name = "type")
+    private String type;
+
+
+
+    public FileInfo(String name, String md5, String path, Date uploadTime, String type) {
+        this.name = name;
+        this.md5 = md5;
+        this.path = path;
+        this.uploadTime = uploadTime;
+        this.type = type;
+    }
+}
