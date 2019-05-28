f203f663b11f2e90e69c12d43a1beff36bebe5d3
huifer <1187982580@qq.com>
2019-05-28 17:21:33 +0800

    自定义配置仓储

---
+++ microservice-core/cloud/cloud-config-server/src/main/java/com/huifer/cloud/config/myrespository/MyEnvironmentRepository.java
@@ -0,0 +1,45 @@
+package com.huifer.cloud.config.myrespository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>Title : MyEnvironmentRepository </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-05-28
+ */
+
+@Configuration
+public class MyEnvironmentRepository {
+
+    @Bean
+    public EnvironmentRepository environmentRepository() {
+        EnvironmentRepository environmentRepository = new EnvironmentRepository() {
+            @Override
+            public Environment findOne(String application, String profile, String label) {
+                Environment environment = new Environment("default", profile);
+                List<PropertySource> propertySources = environment.getPropertySources();
+
+                Map<String, String> source = new HashMap<>();
+                source.put("MyEnvironmentRepository", "hello,world");
+
+                PropertySource propertySource = new PropertySource("new_property", source);
+
+                propertySources.add(propertySource);
+                return environment;
+            }
+        };
+
+        return environmentRepository;
+    }
+
+
+}
