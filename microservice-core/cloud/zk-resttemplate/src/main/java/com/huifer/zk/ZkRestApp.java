351c03f9cb74210885b4506d4ca6e68b777ff775
huifer <1187982580@qq.com>
2019-05-30 09:26:51 +0800

    ribbon

--- microservice-core/cloud/zk-resttemplate/src/main/java/com/huifer/zk/ZkRestApp.java
+++ microservice-core/cloud/zk-resttemplate/src/main/java/com/huifer/zk/ZkRestApp.java
@@ -3,6 +3,7 @@ package com.huifer.zk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * <p>Title : ZkRestApp </p>
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class ZkRestApp {
 
     public static void main(String[] args) {
