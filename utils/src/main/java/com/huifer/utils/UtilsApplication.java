3e61581a5c6faf470808cd66caf632b31dbdc788
wangtao <1047805126@qq.com>
2019-07-26 19:24:30 +0800

    feat(数据库generator工具):

---
+++ utils/src/main/java/com/huifer/utils/UtilsApplication.java
@@ -0,0 +1,13 @@
+package com.huifer.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilsApplication.class, args);
+	}
+
+}
---
+++ utils/src/main/java/com/huifer/utils/mapper/GeneratorMapper.java
@@ -0,0 +1,45 @@
+package com.huifer.utils.mapper;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author: wang
+ * @description: 表机构查询语句
+ */
+@Mapper
+public interface GeneratorMapper {
+
+    /**
+     * 查询表结构
+     *
+     * @param tableName 表名,输入""查询所有
+     * @return
+     */
+    List<Map<String, Object>> findTableDesc(
+            @Param("tableName") String tableName,
+            @Param("offset") int offset,
+            @Param("limit") int limit
+
+    );
+
+    /**
+     * 统计表数量
+     *
+     * @param tableName
+     * @return
+     */
+    int findTableDescCount(
+            @Param("tableName") String tableName
+    );
+
+
+    Map<String, String> queryTable(@Param("tableName") String tableName);
+
+
+    List<Map<String, String>> queryColumns(@Param("tableName") String tableName);
+
+
+}
