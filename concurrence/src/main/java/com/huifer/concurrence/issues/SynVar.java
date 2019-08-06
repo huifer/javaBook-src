bcb1be2c6fd410302b20582141a20582930a571b
huifer <1187982580@qq.com>
2019-08-06 16:59:28 +0800

    多线程

---
+++ concurrence/src/main/java/com/huifer/concurrence/issues/SynVar.java
@@ -0,0 +1,65 @@
+package com.huifer.concurrence.issues;
+
+/**
+ * 线程间数据共享
+ */
+public class SynVar {
+
+	public static void main(String[] args) throws InterruptedException {
+		Bank bank = new Bank();
+		bank.setMoney(100);
+		bank.setName("fur");
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				bank.addMoney(10);
+				System.out.println("add当前money" + bank.getMoney());
+			}
+		}).start();
+
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				bank.subMoney(10);
+				System.out.println("sub当前money" + bank.getMoney());
+			}
+		}).start();
+
+		Thread.sleep(1000L);
+		System.out.println("======");
+		System.out.println(bank.getMoney());
+	}
+
+	static class Bank {
+		private String name;
+		private int money;
+
+
+		public synchronized void addMoney(int m) {
+			this.money += m;
+		}
+
+		public synchronized void subMoney(int m) {
+			this.money -= m;
+		}
+
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getMoney() {
+			return money;
+		}
+
+		public void setMoney(int money) {
+			this.money = money;
+		}
+	}
+
+}
+
