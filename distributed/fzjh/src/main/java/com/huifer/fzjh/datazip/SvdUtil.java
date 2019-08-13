6d5f9ff83580c35171d7d43bad81e800a870548d
huifer <1187982580@qq.com>
2019-08-13 13:48:39 +0800

    SVD

--- distributed/fzjh/src/main/java/com/huifer/fzjh/datazip/SvdUtil.java
+++ distributed/fzjh/src/main/java/com/huifer/fzjh/datazip/SvdUtil.java
@@ -1,6 +1,7 @@
 package com.huifer.fzjh.datazip;
 
 import Jama.Matrix;
+import Jama.SingularValueDecomposition;
 
 public class SvdUtil {
 
@@ -8,59 +9,41 @@ public class SvdUtil {
 		throw new RuntimeException("this is a util class");
 	}
 
-
-	public static double[][] svd(double[][] inputMatrix, int rank) {
-		Matrix matrix = new Matrix(inputMatrix);
-		int maxRank = matrix.rank();
-		Matrix s = matrix.svd().getS();
-		Matrix u = matrix.svd().getU();
-		Matrix v = matrix.svd().getV();
-
-		if (rank > maxRank) {
-			rank = maxRank;
+	public static void main(String[] args) {
+		double[][] d = {
+				{0, 0, 1},
+				{0, 0, 1},
+				{0, 0, 0},
+		};
+		Matrix matrix = new Matrix(d);
+		SingularValueDecomposition svd1 = matrix.svd();
+		// 奇异值确认: 删除左对角线趋近0的行列
+		Matrix s = svd1.getS();
+		double[][] sArr = s.getArrayCopy();
+		for (int i = 0; i < sArr.length; i++) {
+			double[] doubles = sArr[i];
+			for (int j = 0; j < doubles.length; j++) {
+				double ij = sArr[i][j];
+				if (Math.abs(0 - ij) < 0.00000001) {
+					System.out.println("第" + i + "行");
+					System.out.println("第" + j + "列");
+				}
+			}
+			System.out.println();
 		}
 
-		s = new Matrix(MyMatrix.rankedMatrixS(new MyMatrix(s.getArrayCopy()), rank));
-		u = new Matrix(MyMatrix.rankedMatrixUV(new MyMatrix(u.getArrayCopy()), rank));
-		v = new Matrix(MyMatrix.rankedMatrixUV(new MyMatrix(v.getArrayCopy()), rank));
+//		print(svd1.getS().getArrayCopy());
 
-		MyMatrix composed = new MyMatrix(u.times(s).times(v.transpose()).getArray());
-		return composed.getMatrix();
+
 	}
 
-
-	private static class MyMatrix {
-		private double[][] matrix;
-
-		public MyMatrix(double[][] matrix) {
-			this.matrix = matrix;
-		}
-
-		public int getRowCount() {
-			return this.matrix.length;
-		}
-
-		public static double[][] rankedMatrixS(MyMatrix myMatrix, int rank) {
-			double[][] rankedMatrix = new double[rank][rank];
-			for (int i = 0; i < rank; i++) {
-				double[] rowMatrix = myMatrix.getMatrix()[0];
-				System.arraycopy(rowMatrix, 0, rankedMatrix[i], 0, rank);
+	private static void print(double[][] arrayCopy) {
+		for (double[] doubles : arrayCopy) {
+			for (double aDouble : doubles) {
+				System.out.print(aDouble + "\t");
 			}
-			return rankedMatrix;
-		}
-
-		public static double[][] rankedMatrixUV(MyMatrix myMatrix, int rank) {
-			int rows = myMatrix.getRowCount();
-			double[][] rankedMatrix = new double[rows][rank];
-			for (int i = 0; i < rows; i++) {
-				double[] rowMatrix = myMatrix.getMatrix()[i];
-				System.arraycopy(rowMatrix, 0, rankedMatrix[i], 0, rank);
-			}
-			return rankedMatrix;
-		}
-
-		public double[][] getMatrix() {
-			return matrix;
+			System.out.println();
 		}
 	}
+
 }
