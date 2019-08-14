a79c17f54921aa378e8061c372aac73cecca6e2c
huifer <1187982580@qq.com>
2019-08-14 09:06:58 +0800

    redis

--- microservice-core/spring-boot-redis-demo/src/test/java/com/huifer/redis/config/RedisConfigTest.java
+++ microservice-core/spring-boot-redis-demo/src/test/java/com/huifer/redis/config/RedisConfigTest.java
@@ -9,49 +9,66 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RedisConfigTest {
 
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+	@Autowired
+	private StringRedisTemplate redis;
 
-    @Autowired
-    @Qualifier(value = "redis_2")
-    private RedisTemplate<String, Object> r2;
+	@Test
+	public void a() {
+		StringBuilder platWaringIds = new StringBuilder();
+		for (int i = 0; i < 10; i++) {
+			platWaringIds.append(i + ",");
+		}
+		redis.opsForValue().set("plat:waring:", platWaringIds.toString());
+		String s = redis.opsForValue().get("plat:waring:");
+		if (s != null && s.contains(String.valueOf(1))) {
+			System.out.println(s);
+		}
+	}
 
-    @Test
-    public void testSet() {
-        Student student = new Student();
-        student.setAge(10);
-        student.setName("zhangsan");
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
-        redisTemplate.opsForValue().set("student-02", student);
+	@Autowired
+	@Qualifier(value = "redis_2")
+	private RedisTemplate<String, Object> r2;
 
-        Student s = new Student();
-        Object o = redisTemplate.opsForValue().get("student-02");
-        BeanUtils.copyProperties(o, s);
-        Assert.assertTrue(s.equals(student));
+	@Test
+	public void testSet() {
+		Student student = new Student();
+		student.setAge(10);
+		student.setName("zhangsan");
 
-    }
+		redisTemplate.opsForValue().set("student-02", student);
+
+		Student s = new Student();
+		Object o = redisTemplate.opsForValue().get("student-02");
+		BeanUtils.copyProperties(o, s);
+		Assert.assertTrue(s.equals(student));
+
+	}
 
 
-    @Test
-    public void testSet2() {
-        Student student = new Student();
-        student.setAge(10);
-        student.setName("wangwu");
+	@Test
+	public void testSet2() {
+		Student student = new Student();
+		student.setAge(10);
+		student.setName("wangwu");
 
-        r2.opsForValue().set("student-03", student);
+		r2.opsForValue().set("student-03", student);
 
-        Student s = new Student();
-        Object o = r2.opsForValue().get("student-03");
-        BeanUtils.copyProperties(o, s);
-        Assert.assertTrue(s.equals(student));
+		Student s = new Student();
+		Object o = r2.opsForValue().get("student-03");
+		BeanUtils.copyProperties(o, s);
+		Assert.assertTrue(s.equals(student));
 
-    }
+	}
 
 
 }
