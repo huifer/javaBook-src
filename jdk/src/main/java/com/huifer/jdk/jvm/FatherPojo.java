f08e71f44a045b0bece4c751257ff3508289962b
huifer <1187982580@qq.com>
2019-08-20 18:19:13 +0800

    问答 :robot:

---
+++ jdk/src/main/java/com/huifer/jdk/jvm/FatherPojo.java
@@ -0,0 +1,20 @@
+package com.huifer.jdk.jvm;
+
+public class FatherPojo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FatherPojo() {
+    }
+
+    public FatherPojo(String name) {
+        this.name = name;
+    }
+}
---
+++ jdk/src/main/java/com/huifer/jdk/jvm/SonPojo.java
@@ -0,0 +1,14 @@
+package com.huifer.jdk.jvm;
+
+public class SonPojo extends FatherPojo {
+
+    private int age;
+
+    public SonPojo() {
+    }
+
+    public SonPojo( int age) {
+        super();
+        this.age = age;
+    }
+}
