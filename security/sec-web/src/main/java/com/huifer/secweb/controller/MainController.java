dbf5480410578ca6f90e43edc4021e6ba3889918
huifer <huifer97@163.com>
2019-11-10 10:31:30 +0800

    环境搭建

---
+++ security/sec-web/src/main/java/com/huifer/secweb/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.huifer.secweb.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 描述:
+ *
+ * @author: huifer
+ * @date: 2019-11-10
+ */
+@Controller
+public class MainController {
+}
