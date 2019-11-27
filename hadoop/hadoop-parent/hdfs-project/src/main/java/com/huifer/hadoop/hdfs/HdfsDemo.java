bd09ef6d578df73d4f06445bb04f6c8f88d6071b
huifer <huifer97@163.com>
2019-11-27 17:54:18 +0800

    连不上ali服务器的hdfs

--- hadoop/hadoop-parent/hdfs-project/src/main/java/com/huifer/hadoop/hdfs/HdfsDemo.java
+++ hadoop/hadoop-parent/hdfs-project/src/main/java/com/huifer/hadoop/hdfs/HdfsDemo.java
@@ -4,23 +4,27 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
+import java.net.URI;
 
 import java.io.InputStream;
-import java.net.URI;
 
 public class HdfsDemo {
     public static void main(String[] args) throws Exception {
 
-        Configuration entries = new Configuration();
-        String url = "hdfs://:9000/hh/input/1.txt";
+
+        String url = "hdfs://47.98.225.144:9000/hh/input/1.txt";
         Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(URI.create(url),conf);
+//        conf.set("fs.defaultFS", "hdfs://47.98.225.144:9000");
+
+        FileSystem huifer = FileSystem.get(new URI("hdfs://47.98.225.144:9000"), conf, "huifer");
+
+        FileSystem fileSystem = FileSystem.get(conf);
         InputStream in = null;
-        try
-        {
-            in = fs.open(new Path(url));
-            IOUtils.copyBytes(in,System.out,4096,false);
-        }finally {
+
+        try {
+            in = fileSystem.open(new Path(url));
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
             IOUtils.closeStream(in);
         }
     }
