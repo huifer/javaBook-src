5840f77b6b7a8527b618bd0d9d68cb20ff7ef735
huifer <huifer97@163.com>
2019-12-05 13:00:19 +0800

    动态实体

---
+++ utils/src/main/java/com/huifer/utils/controller/BaseBase.java
@@ -0,0 +1,10 @@
+package com.huifer.utils.controller;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class BaseBase {
+}
---
+++ utils/src/main/java/com/huifer/utils/controller/StuFat.java
@@ -0,0 +1,13 @@
+package com.huifer.utils.controller;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+public class StuFat {
+    private String username;
+    private String pwd;
+    private String hcName;
+}
