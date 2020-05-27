29ec070667a476ea1847382d9949a9837c90914f
huifer <huifer97@163.com>
2020-05-27 10:14:37 +0800

    rbac

---
+++ project/rbac/src/main/java/org/huifer/rbac/RbacApplication.java
@@ -0,0 +1,13 @@
+package org.huifer.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacApplication.class, args);
+    }
+
+}
