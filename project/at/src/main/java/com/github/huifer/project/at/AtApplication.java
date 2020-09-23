402be6e22701f7e237acc14ae2c876ac8ef38874
huifer <huifer97@163.com>
2020-09-23 10:07:10 +0800

    AT

---
+++ project/at/src/main/java/com/github/huifer/project/at/AtApplication.java
@@ -0,0 +1,13 @@
+package com.github.huifer.project.at;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtApplication.class, args);
+	}
+
+}
