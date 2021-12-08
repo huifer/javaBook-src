064fc76c61334f5a7db50cf8fd192e3a0b5aff95
huifer <huifer97@163.com>
2021-12-08 19:21:01 +0800

    fix: cli

---
+++ micronaut/cli/src/test/java/cli/CliCommandTest.java
@@ -0,0 +1,28 @@
+package cli;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CliCommandTest {
+
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[] { "-v" };
+            PicocliRunner.run(CliCommand.class, ctx, args);
+
+            // cli
+            assertTrue(baos.toString().contains("Hi!"));
+        }
+    }
+}
