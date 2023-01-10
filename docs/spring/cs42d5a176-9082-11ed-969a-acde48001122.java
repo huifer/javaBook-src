22783f480ac677bbadecd3ba581aded148243d6b
Zen Huifer <huifer97@163.com>
2023-01-10 09:01:06 +0800

    write a line into test.file

---
+++ docs/spring/cs42d5a176-9082-11ed-969a-acde48001122.java
@@ -0,0 +1,28 @@
+package com.huifer.kafka.utils;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.util.Properties;
+
+/**
+ * <p>Title : KafkaConsumerInit </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-06-25
+ */
+public class KafkaConsumerInit {
+    public static Properties conf() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.106:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaConsumerDemo-java");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return properties;
+    }
+}
