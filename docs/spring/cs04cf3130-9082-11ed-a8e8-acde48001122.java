cffdad1a486b71a0c7c4c6f66f6828aa961a439c
Zen Huifer <huifer97@163.com>
2023-01-10 08:59:22 +0800

    write a line into test.file

---
+++ docs/spring/cs04cf3130-9082-11ed-a8e8-acde48001122.java
@@ -0,0 +1,18 @@
+package com.huifer.bean;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 描述:
+ * 老师
+ *
+ * @author huifer
+ * @date 2019-03-02
+ */
+@Data
+@NoArgsConstructor
+public class Teacher {
+    private String tname;
+
+}
