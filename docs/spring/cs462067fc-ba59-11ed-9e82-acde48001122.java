9e541403f21d31524f3a4dc0b8855d84b80eb7ff
Zen Huifer <huifer97@163.com>
2023-03-04 14:53:33 +0800

    write a line into test.file

---
+++ docs/spring/cs462067fc-ba59-11ed-9e82-acde48001122.java
@@ -0,0 +1,13 @@
+package com.huifer.restsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSecApplication.class, args);
+    }
+
+}
