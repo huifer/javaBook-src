3e907d2b747037359f764edd6dc646782ab50d66
Zen Huifer <huifer97@163.com>
2023-05-04 09:36:04 +0800

    write a line into test.file

---
+++ docs/spring/cs06b94992-ea1c-11ed-918c-acde48001122.java
@@ -0,0 +1,23 @@
+package com.huifer.note.resource.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/me")
+public class UserController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<Principal> get(final Principal principal) {
+        return ResponseEntity.ok(principal);
+    }
+
+
+
+}
