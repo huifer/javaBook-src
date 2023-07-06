a808a993f93810cb6b02f80aa474b5019efecbb8
Zen Huifer <huifer97@163.com>
2023-07-06 09:36:04 +0800

    write a line into test.file

---
+++ docs/spring/cs78b22804-1b9d-11ee-89ed-acde48001122.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * The annotation that specify an SQL for inserting record(s).
+ *
+ * <p><br>
+ * <b>How to use:</b>
+ * <pre>
+ * public interface UserMapper {
+ *   &#064;Insert("INSERT INTO users (id, name) VALUES(#{id}, #{name})")
+ *   void insert(User user);
+ * }
+ * </pre>
+ * @author Clinton Begin
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Insert {
+    /**
+     * Returns an SQL for inserting record(s).
+     *
+     * @return an SQL for inserting record(s)
+     */
+    String[] value();
+}
