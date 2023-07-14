bd9ffdecedfc3095ec8935e86060d52fdac7d2a5
Zen Huifer <huifer97@163.com>
2023-07-14 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cscb61d30c-21e6-11ee-937b-acde48001122.java
@@ -0,0 +1,46 @@
+//package com.github.huifer.full.shiro.repo;
+//
+//import com.github.huifer.full.shiro.FullShiroApp;
+//import java.util.ArrayList;
+//import java.util.List;
+//import javax.persistence.criteria.CriteriaBuilder;
+//import javax.persistence.criteria.CriteriaQuery;
+//import javax.persistence.criteria.Join;
+//import javax.persistence.criteria.JoinType;
+//import javax.persistence.criteria.Predicate;
+//import javax.persistence.criteria.Root;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.jpa.domain.Specification;
+//
+//@SpringBootTest(classes = {FullShiroApp.class})
+//class ShiroUserEntityRepositoryTest {
+//
+//  @Autowired
+//  private ShiroUserEntityRepository shiroUserEntityRepository;
+//
+//  @Test
+//  void findShiroUserEntityByLoginName() {
+//  }
+//
+//  @Test
+//  void queryAllByEmailAndUsernameAndGender() {
+//    PageRequest pageable = PageRequest.of(1, 1);
+//    Page<ShiroUserEntity> all = shiroUserEntityRepository
+//        .findAll(new Specification<ShiroUserEntity>() {
+//          @Override
+//          public Predicate toPredicate(
+//              Root<ShiroUserEntity> root,
+//              CriteriaQuery<?> query,
+//              CriteriaBuilder criteriaBuilder) {
+//            List<Predicate> list = new ArrayList<Predicate>();
+//            list.add(criteriaBuilder.like(root.get("username"), "%%"));
+//            return null;
+//          }
+//        }, pageable);
+//    System.out.println();
+//  }
+//}
