961a303d2c77643b18a80887c1fdeab0bf2a33c7
Zen Huifer <huifer97@163.com>
2023-07-24 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cs73f171b6-29c2-11ee-962e-acde48001122.java
@@ -0,0 +1,47 @@
+package com.huifer.concurrence.ch1;
+
+/**
+ * <p>Title : ThreadDemo001 </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-03-25
+ */
+public class ThreadDemo001 {
+
+    public static void main(String[] args) {
+        Runnable r1 = () -> {
+            Thread thread = Thread.currentThread();
+            System.out.printf("runnable 内线程：%s 是否存活: %s 状态 %s",
+                    thread.getName(),
+                    thread.isAlive(),
+                    thread.getState()
+            );
+            System.out.println();
+
+        };
+
+        Thread t1 = new Thread(r1, "thread-1");
+        Thread t2 = new Thread(r1, "thread-2");
+
+        // 设置t1 作为守护线程
+        t1.setDaemon(true);
+
+        System.out.printf("线程：%s 是否存活: %s 状态 %s",
+                t1.getName(),
+                t1.isAlive(),
+                t1.getState()
+        );
+        System.out.println();
+
+        System.out.printf("线程：%s 是否存活: %s 状态 %s",
+                t2.getName(),
+                t2.isAlive(),
+                t2.getState()
+        );
+        System.out.println();
+        t1.start();
+        t2.start();
+    }
+
+}
