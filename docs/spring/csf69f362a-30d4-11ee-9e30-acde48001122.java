9dcb7e6eda33e61ae597c4d76c2cab5e2fa79601
Zen Huifer <huifer97@163.com>
2023-08-02 09:36:14 +0800

    write a line into test.file

---
+++ docs/spring/csf69f362a-30d4-11ee-9e30-acde48001122.java
@@ -0,0 +1,101 @@
+package com.huifer.zk.perception;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ */
+public class ZkProvider {
+
+    private final int timeout = 2000;
+    private ZooKeeper zooKeeper = null;
+    /**
+     * 地址列表用","分割 server1:port,server2:port
+     */
+    private String connects = "localhost:2181";
+    /**
+     * 父节点
+     */
+    private String parentNode = "/server";
+
+    public ZkProvider() {
+        initZk();
+    }
+
+
+    public ZkProvider(ZooKeeper zooKeeper, String connects) {
+        this.zooKeeper = zooKeeper;
+        this.connects = connects;
+        initZk();
+    }
+
+    public ZooKeeper getZooKeeper() {
+        return zooKeeper;
+    }
+
+    /**
+     * 节点递归输出
+     *
+     * @param path
+     * @throws Exception
+     */
+    public void ls(String path) throws Exception {
+        System.out.println(path);
+        List<String> list = this.zooKeeper.getChildren(path, null);
+        //判断是否有子节点
+        if (list.isEmpty() || list == null) {
+            return;
+        }
+        for (String s : list) {
+            //判断是否为根目录
+            if (path.equals("/")) {
+                ls(path + s);
+            } else {
+                ls(path + "/" + s);
+            }
+        }
+    }
+
+    /**
+     * 添加节点
+     *
+     * @param hostname
+     * @throws Exception
+     */
+    public void register(String hostname) {
+        String createNode = null;
+        try {
+            createNode = zooKeeper.create(parentNode, hostname.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            System.out.println(hostname + "  is online:" + createNode);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(String hostname) {
+        try {
+            zooKeeper.delete(hostname, 0);
+        } catch (InterruptedException | KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 初始化zookeeper
+     */
+    protected void initZk() {
+        try {
+            zooKeeper = new ZooKeeper(connects, timeout, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
