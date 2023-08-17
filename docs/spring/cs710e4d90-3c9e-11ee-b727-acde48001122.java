1f25cd4fa2796a7e1e541196f142e5f757495262
Zen Huifer <huifer97@163.com>
2023-08-17 09:36:12 +0800

    write a line into test.file

---
+++ docs/spring/cs710e4d90-3c9e-11ee-b727-acde48001122.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.type;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class BigDecimalTypeHandlerTest extends BaseTypeHandlerTest {
+
+    private static final TypeHandler<BigDecimal> TYPE_HANDLER = new BigDecimalTypeHandler();
+
+    @Override
+    @Test
+    public void shouldSetParameter() throws Exception {
+        TYPE_HANDLER.setParameter(ps, 1, new BigDecimal(1), null);
+        verify(ps).setBigDecimal(1, new BigDecimal(1));
+    }
+
+    @Override
+    @Test
+    public void shouldGetResultFromResultSetByName() throws Exception {
+        when(rs.getBigDecimal("column")).thenReturn(new BigDecimal(1));
+        assertEquals(new BigDecimal(1), TYPE_HANDLER.getResult(rs, "column"));
+        verify(rs, never()).wasNull();
+    }
+
+    @Override
+    public void shouldGetResultNullFromResultSetByName() throws Exception {
+        // Unnecessary
+    }
+
+    @Override
+    @Test
+    public void shouldGetResultFromResultSetByPosition() throws Exception {
+        when(rs.getBigDecimal(1)).thenReturn(new BigDecimal(1));
+        assertEquals(new BigDecimal(1), TYPE_HANDLER.getResult(rs, 1));
+        verify(rs, never()).wasNull();
+    }
+
+    @Override
+    public void shouldGetResultNullFromResultSetByPosition() throws Exception {
+        // Unnecessary
+    }
+
+    @Override
+    @Test
+    public void shouldGetResultFromCallableStatement() throws Exception {
+        when(cs.getBigDecimal(1)).thenReturn(new BigDecimal(1));
+        assertEquals(new BigDecimal(1), TYPE_HANDLER.getResult(cs, 1));
+        verify(cs, never()).wasNull();
+    }
+
+    @Override
+    public void shouldGetResultNullFromCallableStatement() throws Exception {
+        // Unnecessary
+    }
+
+}
