980dcfb789d31c05b0af8fa694b1e3ba9f0cdd0f
Zen Huifer <huifer97@163.com>
2023-08-27 16:56:42 +0800

    write a line into test.file

---
+++ docs/spring/cs97b76eb0-44b7-11ee-b591-acde48001122.java
@@ -0,0 +1,35 @@
+package com.huifer.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>Title : SpringBootBuildApplication </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-05-22
+ */
+@SpringBootApplication
+public class SpringBootBuildApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication springApplication = new SpringApplication(
+                SpringBootBuildApplication.class);
+
+        Map<String, Object> properties = new HashMap<>();
+        // server.port=0 系统随机分发一个未使用的端口
+        properties.put("server.port", 0);
+        springApplication.setDefaultProperties(properties);
+
+
+        springApplication.run(args);
+
+
+    }
+
+}
