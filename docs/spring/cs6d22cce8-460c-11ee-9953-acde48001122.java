259bcd1dfedc5a1cad0979f958be68e5ffd37590
Zen Huifer <huifer97@163.com>
2023-08-29 09:36:09 +0800

    write a line into test.file

---
+++ docs/spring/cs6d22cce8-460c-11ee-9953-acde48001122.java
@@ -0,0 +1,31 @@
+package com.huifer.kafka.core.bean;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+public class AnnotationTranversor<C, K, V> {
+    Class<? extends Object> clazz;
+
+    public AnnotationTranversor(Class<? extends Object> clazz) {
+        this.clazz = clazz;
+    }
+
+    public Map<C, Map<K, V>> tranverseAnnotation(
+            AnnotationHandler<C, K, V> annotationHandler) {
+        TranversorContext<C, K, V> ctx = new TranversorContext<C, K, V>();
+
+        for (Annotation annotation : clazz.getAnnotations()) {
+            annotationHandler.handleClassAnnotation(clazz, annotation, ctx);
+        }
+
+        for (Method method : clazz.getMethods()) {
+            for (Annotation annotation : method.getAnnotations()) {
+                annotationHandler.handleMethodAnnotation(clazz, method,
+                        annotation, ctx);
+            }
+        }
+
+        return ctx.getData();
+    }
+}
