e60779012da364a9bc6aa0f2937fd0736cdbd4d8
Zen Huifer <huifer97@163.com>
2023-09-12 09:36:10 +0800

    write a line into test.file

---
+++ docs/spring/csbdc3a7fc-510c-11ee-b96f-acde48001122.java
@@ -0,0 +1,67 @@
+package com.huifer.netty.io.bio;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * <p>Title : BioClient </p>
+ * <p>Description : Bio Client</p>
+ *
+ * @author huifer
+ * @date 2019-06-20
+ */
+@Slf4j
+public class BioClient {
+
+    public static int SERVER_PORT = 10086;
+
+    private static String SERVER_IP = "127.0.0.1";
+
+    public static void send(String exp) {
+        send(SERVER_IP, SERVER_PORT, exp);
+    }
+
+    private static void send(String serverIp, int serverPort, String exp) {
+        log.info("exp : " + exp);
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket(serverIp, serverPort);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(exp);
+            log.info("返回结果 " + in.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getLocalizedMessage());
+
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ docs/spring/csbf56e1ce-510c-11ee-b96f-acde48001122.java
@@ -0,0 +1,44 @@
+package com.huifer.jdk.equals;
+
+/**
+ * <p>Title : Run </p>
+ * <p>Description : == 和 equals</p>
+ *
+ * @author huifer
+ * @date 2019-05-24
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        // == 对于基本类型 int long  double float boolean short 来说对比较的是值是否相等
+        int i = 1;
+        int j = 1;
+        System.out.println(i == j);
+
+        // == 对于引用类型 比较的是引用是否相同
+        // String 的创建方式 直接等于字符串 或者 new String(字符串)
+        // 直接创建的字符串他们的引用地址相同 ， 而 new 创建的字符串是开辟新的内存空间
+        String a = "b";
+        String b = "b";
+        System.out.println(a == b);
+        String c = new String("b");
+        System.out.println(a == c);
+
+        // equlas
+        System.out.println(a.equals(c));
+
+        Eq eq1 = new Eq(1);
+        Eq eq2 = new Eq(1);
+
+        System.out.println(eq1.equals(eq2));
+    }
+
+
+    private static class Eq {
+        public int anInt;
+
+        public Eq(int anInt) {
+            this.anInt = anInt;
+        }
+    }
+}
