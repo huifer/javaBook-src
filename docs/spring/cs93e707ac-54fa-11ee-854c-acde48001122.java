cd2ad0ad4bfa201f45f13e3b82019290f3f3fe1c
Zen Huifer <huifer97@163.com>
2023-09-17 09:36:13 +0800

    write a line into test.file

---
+++ docs/spring/cs93e707ac-54fa-11ee-854c-acde48001122.java
@@ -0,0 +1,40 @@
+package com.huifer.activermq.producer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * <p>Title : JMSTopicProducer </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-06-18
+ */
+public class JMSTopicProducer {
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+                "tcp://192.168.1.108:61616");
+        ((ActiveMQConnectionFactory) connectionFactory).setAlwaysSyncSend(true);
+        Connection connection = connectionFactory.createConnection();
+        // 启动连接
+        connection.start();
+        // 创建会话
+        Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+        Topic testTopic = session.createTopic("testTopic");
+
+        MessageProducer producer = session.createProducer(testTopic);
+
+        producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+        TextMessage hello_topic = session.createTextMessage("hello topic");
+        producer.send(hello_topic);
+
+        session.commit();
+        session.close();
+        session.rollback();
+        connection.close();
+
+    }
+}
