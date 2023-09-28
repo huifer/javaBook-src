2eb543f72183e22e907d21394351af7d8cd87687
Zen Huifer <huifer97@163.com>
2023-09-28 09:36:05 +0800

    write a line into test.file

---
+++ docs/spring/cs63292fa2-5d9f-11ee-9a72-acde48001122.java
@@ -0,0 +1,4 @@
+package org.huifer.rbac.service;
+
+public interface IMenuService {
+}
---
+++ docs/spring/cs636f16fc-5d9f-11ee-9a72-acde48001122.java
@@ -0,0 +1,22 @@
+package com.huifer.design.prototype;
+
+import java.io.Serializable;
+
+/**
+ * <p>Title : Tag </p>
+ * <p>Description :  </p>
+ *
+ * @author huifer
+ * @date 2019-05-16
+ */
+public class Tag implements Serializable {
+
+    private static final long serialVersionUID = -1732973065571551933L;
+    public String f;
+
+    public Tag(String f) {
+        this.f = f;
+    }
+
+
+}
