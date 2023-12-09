7a8f6991ad2ae9033ef645cdfa7522a2aec0c6a1
Zen Huifer <huifer97@163.com>
2023-12-09 15:30:39 +0800

    write a line into test.file

---
+++ docs/spring/csd4ed0cbe-9664-11ee-a63d-acde48001122.java
@@ -0,0 +1,51 @@
+package com.huifer.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JavaBean implements BeanPostProcessor {
+    private String name;
+
+    public JavaBean() {
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("BeanLive.xml");
+        JavaBean javaBean = (JavaBean) ctx.getBean("javaBean");
+        javaBean.getName();
+        ctx.close();
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("前置");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("后置处理");
+        return bean;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println("setter 方法");
+        this.name = name;
+    }
+
+    public void init() {
+        System.out.println("执行初始化方法");
+    }
+
+
+    public void close() {
+        System.out.println("执行摧毁方法");
+    }
+}
---
+++ docs/spring/csd57898e2-9664-11ee-a63d-acde48001122.java
@@ -0,0 +1,23 @@
+package com.github.huifer.tuc.model;
+
+public class CompanyEntity {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
