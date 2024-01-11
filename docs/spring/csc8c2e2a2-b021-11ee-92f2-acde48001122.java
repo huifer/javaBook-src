0e44d7b4e4a6cf548db0198d7b94795f86d40699
Zen Huifer <huifer97@163.com>
2024-01-11 09:36:09 +0800

    write a line into test.file

---
+++ docs/spring/csc8c2e2a2-b021-11ee-92f2-acde48001122.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.reflection;
+
+import org.apache.ibatis.reflection.factory.ObjectFactory;
+import org.apache.ibatis.reflection.property.PropertyTokenizer;
+import org.apache.ibatis.reflection.wrapper.*;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Clinton Begin
+ */
+public class MetaObject {
+
+    /**
+     * 原始的数据对象,初始化时的对象
+     */
+    private final Object originalObject;
+    /**
+     * 对象包装
+     */
+    private final ObjectWrapper objectWrapper;
+    /**
+     * object 工厂
+     */
+    private final ObjectFactory objectFactory;
+    /**
+     * object
+     */
+    private final ObjectWrapperFactory objectWrapperFactory;
+    /**
+     * 反射工程
+     */
+    private final ReflectorFactory reflectorFactory;
+
+    private MetaObject(Object object, ObjectFactory objectFactory, ObjectWrapperFactory objectWrapperFactory, ReflectorFactory reflectorFactory) {
+        this.originalObject = object;
+        this.objectFactory = objectFactory;
+        this.objectWrapperFactory = objectWrapperFactory;
+        this.reflectorFactory = reflectorFactory;
+
+        // 根据object不同实例进行不同的实例化方式
+        if (object instanceof ObjectWrapper) {
+            this.objectWrapper = (ObjectWrapper) object;
+        } else if (objectWrapperFactory.hasWrapperFor(object)) {
+            this.objectWrapper = objectWrapperFactory.getWrapperFor(this, object);
+        } else if (object instanceof Map) {
+            this.objectWrapper = new MapWrapper(this, (Map) object);
+        } else if (object instanceof Collection) {
+            this.objectWrapper = new CollectionWrapper(this, (Collection) object);
+        } else {
+            this.objectWrapper = new BeanWrapper(this, object);
+        }
+    }
+
+    public static MetaObject forObject(Object object, ObjectFactory objectFactory, ObjectWrapperFactory objectWrapperFactory, ReflectorFactory reflectorFactory) {
+        if (object == null) {
+            return SystemMetaObject.NULL_META_OBJECT;
+        } else {
+            return new MetaObject(object, objectFactory, objectWrapperFactory, reflectorFactory);
+        }
+    }
+
+    public ObjectFactory getObjectFactory() {
+        return objectFactory;
+    }
+
+    public ObjectWrapperFactory getObjectWrapperFactory() {
+        return objectWrapperFactory;
+    }
+
+    public ReflectorFactory getReflectorFactory() {
+        return reflectorFactory;
+    }
+
+    public Object getOriginalObject() {
+        return originalObject;
+    }
+
+    public String findProperty(String propName, boolean useCamelCaseMapping) {
+        return objectWrapper.findProperty(propName, useCamelCaseMapping);
+    }
+
+    public String[] getGetterNames() {
+        return objectWrapper.getGetterNames();
+    }
+
+    public String[] getSetterNames() {
+        return objectWrapper.getSetterNames();
+    }
+
+    public Class<?> getSetterType(String name) {
+        return objectWrapper.getSetterType(name);
+    }
+
+    public Class<?> getGetterType(String name) {
+        return objectWrapper.getGetterType(name);
+    }
+
+    public boolean hasSetter(String name) {
+        return objectWrapper.hasSetter(name);
+    }
+
+    public boolean hasGetter(String name) {
+        return objectWrapper.hasGetter(name);
+    }
+
+    /**
+     * 获取value
+     * @param name 属性值名称
+     * @return
+     */
+    public Object getValue(String name) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            MetaObject metaValue = metaObjectForProperty(prop.getIndexedName());
+            if (metaValue == SystemMetaObject.NULL_META_OBJECT) {
+                // 判断是否是空的metaObject
+                return null;
+            } else {
+                return metaValue.getValue(prop.getChildren());
+            }
+        } else {
+            return objectWrapper.get(prop);
+        }
+    }
+
+    /**
+     * metaObject 设置属性值方法
+     * {name:value}
+     *
+     * @param name  属性值名称
+     * @param value 属性值
+     */
+    public void setValue(String name, Object value) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            // 获取属性实例
+            MetaObject metaValue = metaObjectForProperty(prop.getIndexedName());
+            if (metaValue == SystemMetaObject.NULL_META_OBJECT) {
+                if (value == null) {
+                    // value 空则返回
+                    // don't instantiate child path if value is null
+                    return;
+                } else {
+                    // 创建属性值
+                    metaValue = objectWrapper.instantiatePropertyValue(name, prop, objectFactory);
+                }
+            }
+
+            metaValue.setValue(prop.getChildren(), value);
+        } else {
+            objectWrapper.set(prop, value);
+        }
+    }
+
+    public MetaObject metaObjectForProperty(String name) {
+        Object value = getValue(name);
+        return MetaObject.forObject(value, objectFactory, objectWrapperFactory, reflectorFactory);
+    }
+
+    public ObjectWrapper getObjectWrapper() {
+        return objectWrapper;
+    }
+
+    public boolean isCollection() {
+        return objectWrapper.isCollection();
+    }
+
+    public void add(Object element) {
+        objectWrapper.add(element);
+    }
+
+    public <E> void addAll(List<E> list) {
+        objectWrapper.addAll(list);
+    }
+
+}
