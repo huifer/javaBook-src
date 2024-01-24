24492fb24ac0a5367329673cd6d7cc03310590de
Zen Huifer <huifer97@163.com>
2024-01-24 09:36:11 +0800

    write a line into test.file

---
+++ docs/spring/csf252d41a-ba58-11ee-9b1f-acde48001122.java
@@ -0,0 +1,45 @@
+package com.huifer.kafka.application.ttl;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * <p>Title : Consumer </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-06-27
+ */
+public class Consumer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.106:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ttl-group");
+        props.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,
+                TTLConsumerInterceptor.class.getName());
+
+        KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("ttl"));
+
+        while (true) {
+            ConsumerRecords<Integer, String> records = consumer.poll(1000);
+            for (ConsumerRecord<Integer, String> record : records) {
+                System.out
+                        .println(record.partition() + ":" + record.offset() + ":" + record.value());
+            }
+        }
+
+    }
+
+}
