df71205b8f19144e8203d7813471c5d962653aad
Zen Huifer <huifer97@163.com>
2024-01-31 09:36:08 +0800

    write a line into test.file

---
+++ docs/spring/cs1a70b446-bfd9-11ee-b920-acde48001122.java
@@ -0,0 +1,14 @@
+package com.huifer.spring.controller;
+
+/**
+ * <p>Title : Controller </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-05-22
+ */
+@org.springframework.stereotype.Controller
+public class T2Controller {
+
+
+}
