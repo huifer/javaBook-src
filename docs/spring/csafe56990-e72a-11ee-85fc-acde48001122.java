883244dd2680d43e7d7982eaf61ad453b6fd4883
Zen Huifer <huifer97@163.com>
2024-03-21 10:28:22 +0800

    write a line into test.file

---
+++ docs/spring/csafe56990-e72a-11ee-85fc-acde48001122.java
@@ -0,0 +1,13 @@
+package com.github.huifer.full.shiro.model.req.dept;
+
+import lombok.Data;
+
+@Data
+public class DeptUpdateParam {
+
+  private String name;
+  private Integer pid;
+  private Integer mainUser;
+
+
+}
