04a71820b75f807ce36380fe82246502c772bacd
Zen Huifer <huifer97@163.com>
2024-04-13 09:36:16 +0800

    write a line into test.file

---
+++ docs/spring/cs35d020a0-f936-11ee-8dd7-acde48001122.java
@@ -0,0 +1,16 @@
+package com.huifer.springboot.mysql.repo.two;
+
+import com.huifer.springboot.mysql.pojo.two.StudentTwo;
+
+/**
+ * <p>Title : StudentTwoRepoService </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-06-28
+ */
+public interface StudentTwoRepoService {
+
+    StudentTwo ins();
+
+}
