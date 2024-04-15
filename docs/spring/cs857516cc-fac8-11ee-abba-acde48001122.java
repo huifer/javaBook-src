3662fde5182b13ead214c34c6b68d44c8d22e9e4
Zen Huifer <huifer97@163.com>
2024-04-15 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cs857516cc-fac8-11ee-abba-acde48001122.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.reflection.property;
+
+import org.apache.ibatis.reflection.ReflectionException;
+
+import java.util.Locale;
+
+/**
+ * @author Clinton Begin
+ */
+public final class PropertyNamer {
+
+    private PropertyNamer() {
+        // Prevent Instantiation of Static Class
+    }
+
+
+    /**
+     * 方法转换成属性
+     * @param name
+     * @return
+     */
+    public static String methodToProperty(String name) {
+        if (name.startsWith("is")) {
+            name = name.substring(2);
+        } else if (name.startsWith("get") || name.startsWith("set")) {
+            name = name.substring(3);
+        } else {
+            throw new ReflectionException("Error parsing property name '" + name + "'.  Didn't start with 'is', 'get' or 'set'.");
+        }
+
+        if (name.length() == 1 || (name.length() > 1 && !Character.isUpperCase(name.charAt(1)))) {
+            name = name.substring(0, 1).toLowerCase(Locale.ENGLISH) + name.substring(1);
+        }
+
+        return name;
+    }
+
+    public static boolean isProperty(String name) {
+        return isGetter(name) || isSetter(name);
+    }
+
+    /**
+     * 判断是什么开头的方法
+     *
+     * @param name
+     * @return true get  开头
+     */
+    public static boolean isGetter(String name) {
+        // 在语义上 is 开头的也是get开头的
+        return (name.startsWith("get") && name.length() > 3) || (name.startsWith("is") && name.length() > 2);
+    }
+
+    /**
+     * 判断是什么开头的方法
+     *
+     * @param name
+     * @return true set  开头
+     */
+    public static boolean isSetter(String name) {
+        return name.startsWith("set") && name.length() > 3;
+    }
+
+}
