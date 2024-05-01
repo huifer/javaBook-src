f9230107cd78a4a78b6d9c9d56df473ef74f3e33
Zen Huifer <huifer97@163.com>
2024-05-01 10:52:15 +0800

    write a line into test.file

---
+++ docs/spring/cscfc1ae08-0765-11ef-b6b0-acde48001122.java
@@ -0,0 +1,12 @@
+package com.huifer.design.decorate.cake;
+
+public abstract class BaseCake {
+
+  protected String msg() {
+    return "蛋糕";
+  }
+
+  protected int price() {
+    return 10;
+  }
+}
