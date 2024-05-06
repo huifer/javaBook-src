c246311d31acad05c006138e6747968ec55dd436
Zen Huifer <huifer97@163.com>
2024-05-06 09:45:11 +0800

    write a line into test.file

---
+++ docs/spring/cs4503cb1e-0b4a-11ef-a7f3-f218980c4e71.java
@@ -0,0 +1,16 @@
+package com.huifer.design.factory;
+
+/**
+ * <p>Title : Milk </p>
+ * <p>Description : 牛奶</p>
+ *
+ * @author huifer
+ * @date 2019-05-15
+ */
+public interface Milk {
+
+    /**
+     * 获取一个牛奶的名称
+     */
+    String getName();
+}
