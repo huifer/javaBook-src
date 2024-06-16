f9c3c4d76cb2932fa822aac55cde28dde1a22418
Zen Huifer <huifer97@163.com>
2024-06-16 17:06:33 +0800

    write a line into test.file

---
+++ docs/spring/csb8a8659e-2bbf-11ef-9371-acde48001122.java
@@ -0,0 +1,46 @@
+package com.huifer.rmi.rpc.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * <p>Title : RpcServer </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-06-11
+ */
+public class RpcServer {
+
+
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    /**
+     * 发布一个service
+     *
+     * @param service service {@link com.huifer.rmi.rpc.HelloService}
+     * @param port    port
+     */
+    public void publisher(final Object service, int port) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                executorService.execute(new ProcessorHandler(socket, service));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+    }
+
+}
