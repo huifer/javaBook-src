41c9231d344673313a5548be38169af4e2a72ad3
Zen Huifer <huifer97@163.com>
2024-07-12 09:36:13 +0800

    write a line into test.file

---
+++ docs/spring/cs1e9eeaa6-3fef-11ef-9336-acde48001122.java
@@ -0,0 +1,30 @@
+package com.huifer.kafka.core.handlers;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.huifer.kafka.core.excephandler.ExceptionHandler;
+
+import java.util.List;
+
+public abstract class ObjectMessageHandler<T> extends SafelyMessageHandler {
+
+
+    public ObjectMessageHandler() {
+        super();
+    }
+
+    public ObjectMessageHandler(ExceptionHandler exceptionHandler) {
+        super(exceptionHandler);
+    }
+
+    public ObjectMessageHandler(List<ExceptionHandler> exceptionHandlers) {
+        super(exceptionHandlers);
+    }
+
+    protected void doExecute(String message) {
+        JSONObject jsonObject = JSON.parseObject(message);
+        doExecuteObject(jsonObject);
+    }
+
+    protected abstract void doExecuteObject(JSONObject jsonObject);
+}
