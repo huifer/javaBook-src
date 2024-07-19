f174ce82fb84c07485f65f361405f573b4427379
Zen Huifer <huifer97@163.com>
2024-07-19 09:36:10 +0800

    write a line into test.file

---
+++ docs/spring/cs45f09944-456f-11ef-bec3-acde48001122.java
@@ -0,0 +1,18 @@
+package com.huifer.dao;
+
+/**
+ * 描述:
+ *
+ * @author huifer
+ * @date 2019-03-09
+ */
+public interface BookService {
+    /**
+     * 借书
+     *
+     * @param from  从xxx借出
+     * @param to    借给xxx
+     * @param bname 书名
+     */
+    void loanBook(String from, String to, String bname);
+}
