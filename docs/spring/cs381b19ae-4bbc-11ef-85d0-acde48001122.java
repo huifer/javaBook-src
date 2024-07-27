5db49c37aa4eb3c636dcd73905f0450b6f2904b9
Zen Huifer <huifer97@163.com>
2024-07-27 10:02:05 +0800

    write a line into test.file

---
+++ docs/spring/cs381b19ae-4bbc-11ef-85d0-acde48001122.java
@@ -0,0 +1,27 @@
+package com.huifer.redis.config;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
+
+    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
+        super(listenerContainer);
+    }
+
+    /**
+     * 针对redis数据失效事件，进行数据处理
+     *
+     * @param message
+     * @param pattern
+     */
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String expiredKey = message.toString();
+        System.out.println(expiredKey
+        );
+    }
+}
