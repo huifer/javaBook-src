0482b0e49f501a637684e145f157eed380b96755
Zen Huifer <huifer97@163.com>
2024-08-23 09:36:54 +0800

    write a line into test.file

---
+++ docs/spring/cs13e2195e-60f0-11ef-b443-acde48001122.java
@@ -0,0 +1,28 @@
+package org.huifer.rbac.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.huifer.rbac.entity.req.user.UserAddReq;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @PostMapping("/go")
+    public Object go(@RequestBody @Validated UserAddReq o) {
+
+        Map<String, Object> res = new HashMap<>();
+        res.put("1", "null");
+        res.put("2", null);
+        res.put("3", new Date());
+        res.put("4", o);
+        return res;
+    }
+}
