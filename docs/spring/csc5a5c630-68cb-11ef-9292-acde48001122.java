fd85c7ca493e565998476288c3bd6213b2cabdb1
Zen Huifer <huifer97@163.com>
2024-09-02 09:36:31 +0800

    write a line into test.file

---
+++ docs/spring/csc5a5c630-68cb-11ef-9292-acde48001122.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.submitted.results_id;
+
+import org.apache.ibatis.annotations.*;
+
+public interface Mapper {
+
+    @Results(id = "userResult", value = {
+            @Result(id = true, column = "uid", property = "id"),
+            @Result(column = "name", property = "name")
+    })
+    @Select("select * from users where uid = #{id}")
+    User getUserById(Integer id);
+
+    @ResultMap("userResult")
+    @Select("select * from users where name = #{name}")
+    User getUserByName(String name);
+
+    @Results(id = "userResultConstructor")
+    @ConstructorArgs({
+            @Arg(id = true, column = "uid", javaType = Integer.class),
+            @Arg(column = "name", javaType = String.class)
+    })
+    @Select("select * from users where uid = #{id}")
+    User getUserByIdConstructor(Integer id);
+
+    @ResultMap("userResultConstructor")
+    @Select("select * from users where name = #{name}")
+    User getUserByNameConstructor(String name);
+}
