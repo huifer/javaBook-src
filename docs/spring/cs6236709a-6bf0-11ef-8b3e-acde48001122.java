4a6aeb0d31fb7b82818fe0277900f9e0eff2341b
Zen Huifer <huifer97@163.com>
2024-09-06 09:36:08 +0800

    write a line into test.file

---
+++ docs/spring/cs6236709a-6bf0-11ef-8b3e-acde48001122.java
@@ -0,0 +1,55 @@
+package com.huifer.security.properties;
+
+/**
+ * 描述:
+ *
+ * @author: huifer
+ * @date: 2019-11-17
+ */
+public class ImageCodeProperties {
+    private int width = 67;
+    private int height = 23;
+    private int length = 4;
+    private int expireIn = 60;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getExpireIn() {
+        return expireIn;
+    }
+
+    public void setExpireIn(int expireIn) {
+        this.expireIn = expireIn;
+    }
+}
