4032668b6859ba4e31255b714ce59c2805e248ef
Zen Huifer <huifer97@163.com>
2024-10-10 09:36:08 +0800

    write a line into test.file

---
+++ docs/spring/cs047b6bae-86a8-11ef-86ef-acde48001122.java
@@ -0,0 +1,63 @@
+package com.huifer.mybatis.jdbcProxy;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * 描述:
+ *
+ * @author huifer
+ * @date 2019-02-24
+ */
+public class SqlInvaction implements InvocationHandler {
+
+    Connection connection;
+    PreparedStatement preparedStatement;
+    private SqlSession obj;
+
+    public SqlInvaction(SqlSession obj) {
+        this.obj = obj;
+    }
+
+    @Override
+    public ResultSet invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        init();
+        Field ps = obj.getClass().getDeclaredField("preparedStatement");
+        ps.set(obj, preparedStatement);
+        ResultSet rs = (ResultSet) method.invoke(obj, args);
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String dname = rs.getString("dname");
+            String loc = rs.getString("loc");
+
+            System.out.println(id + "\t" + dname + "\t" + loc);
+        }
+
+        end();
+
+        return rs;
+    }
+
+
+    private void init() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dy_java?serverTimezone=UTC&rewriteBatchedStatements=true&useUnicode=true&characterEncoding=utf8", "root", "root");
+        this.preparedStatement = connection.prepareStatement(" ");
+    }
+
+    private void end() throws Exception {
+        if (this.preparedStatement != null) {
+            this.preparedStatement.close();
+        }
+        if (this.connection != null) {
+            this.connection.close();
+        }
+    }
+
+}
---
+++ docs/spring/cs0513c0e8-86a8-11ef-86ef-acde48001122.java
@@ -0,0 +1,59 @@
+package com.huifer.idgen.my.service;
+
+import com.huifer.idgen.my.service.bean.Id;
+import com.huifer.idgen.my.service.bean.enums.IdType;
+import com.huifer.idgen.my.service.populator.AtomicIdPopulator;
+import com.huifer.idgen.my.service.populator.IdPopulator;
+import com.huifer.idgen.my.service.populator.LockIdPropulator;
+import com.huifer.idgen.my.service.populator.SyncIdPopulator;
+import com.huifer.idgen.my.service.util.CommonUtils;
+
+/**
+ * @author: wang
+ * @description:
+ */
+public class GenIdServiceImpl extends AbstractIdService {
+
+    public static final String SYNC_LOCK = "lock";
+    public static final String ATOMIC = "atomic";
+
+    protected IdPopulator idPopulator;
+
+    public GenIdServiceImpl() {
+        super();
+        initPopulator();
+    }
+
+    /**
+     * {@link IdType} id类型决定时间戳
+     *
+     * @param idType
+     */
+    public GenIdServiceImpl(IdType idType) {
+        super(idType);
+        initPopulator();
+    }
+
+    public GenIdServiceImpl(String idType) {
+        super(idType);
+        initPopulator();
+    }
+
+
+    public void initPopulator() {
+        if (idPopulator != null) {
+        } else if (CommonUtils.isPropKeyOn(SYNC_LOCK)) {
+            idPopulator = new SyncIdPopulator();
+        } else if (CommonUtils.isPropKeyOn(ATOMIC)) {
+            idPopulator = new AtomicIdPopulator();
+        } else {
+            idPopulator = new LockIdPropulator();
+        }
+    }
+
+    @Override
+    protected void populateId(Id id) {
+        idPopulator.populatorId(id, this.idMeta);
+    }
+
+}
