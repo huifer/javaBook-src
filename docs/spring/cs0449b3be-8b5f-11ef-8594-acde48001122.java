eaf4bba8cc0056b37c57122944925b75f6ef70c6
Zen Huifer <huifer97@163.com>
2024-10-16 09:36:11 +0800

    write a line into test.file

---
+++ docs/spring/cs0449b3be-8b5f-11ef-8594-acde48001122.java
@@ -0,0 +1,12 @@
+package com.huifer.dubbo.server.api;
+
+/**
+ * <p>Title : DubboHello </p>
+ * <p>Description : hello</p>
+ *
+ * @author huifer
+ * @date 2019-06-13
+ */
+public interface DubboHello2 {
+    String hello(String msg);
+}
