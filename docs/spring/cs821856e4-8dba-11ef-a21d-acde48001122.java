3e12c1797d7fc15a4e88507d92f2f449d10c4470
Zen Huifer <huifer97@163.com>
2024-10-19 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cs821856e4-8dba-11ef-a21d-acde48001122.java
@@ -0,0 +1,69 @@
+package com.huifer.idgen.my.service.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * @author: wang
+ * @description: ip获取工具
+ */
+@Slf4j
+public class IpUtils {
+
+    private IpUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String getHostIp() {
+        String ip = null;
+        try {
+
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = inetAddresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress()
+                            && !inetAddress.isLinkLocalAddress()
+                            && inetAddress.isSiteLocalAddress()
+                    ) {
+                        ip = inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            log.error("ip 获取异常{}", e);
+        }
+        return ip;
+    }
+
+    public static String getHostName() {
+        String hostName = null;
+        try {
+            Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces();
+            while (en.hasMoreElements()) {
+                NetworkInterface intf = en.nextElement();
+                Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
+                while (enumIpAddr.hasMoreElements()) {
+                    InetAddress inetAddress = enumIpAddr
+                            .nextElement();
+                    if (!inetAddress.isLoopbackAddress()
+                            && !inetAddress.isLinkLocalAddress()
+                            && inetAddress.isSiteLocalAddress()) {
+                        hostName = inetAddress.getHostName();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            log.error("host 获取异常{}", e);
+        }
+        return hostName;
+    }
+
+}
