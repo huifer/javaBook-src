8b506b145a24e646e9884d548d2f563a6ea53b39
Zen Huifer <huifer97@163.com>
2024-11-06 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cs7ce96eaa-9bdf-11ef-9041-acde48001122.java
@@ -0,0 +1,11 @@
+package com.github.huifer.simple.shiro.boot.model.req;
+
+import lombok.Data;
+
+@Data
+public class UserCreateParam {
+
+  private String username;
+  private String password;
+
+}
