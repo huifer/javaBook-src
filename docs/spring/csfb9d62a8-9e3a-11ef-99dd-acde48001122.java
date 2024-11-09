04698e0f7398b2c45962743b6fb21dd342c6c698
Zen Huifer <huifer97@163.com>
2024-11-09 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/csfb9d62a8-9e3a-11ef-99dd-acde48001122.java
@@ -0,0 +1,12 @@
+package com.huifer.design.singleton.nw;
+
+
+public class SimpleSingletonTest {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ExecutorThread());
+        Thread t2 = new Thread(new ExecutorThread());
+        t1.start();
+        t2.start();
+        System.out.println("结束");
+    }
+}
