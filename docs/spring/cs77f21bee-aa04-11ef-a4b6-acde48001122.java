d2befb12aea3008eb50770d4ccdde0de321e9680
Zen Huifer <huifer97@163.com>
2024-11-24 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cs77f21bee-aa04-11ef-a4b6-acde48001122.java
@@ -0,0 +1,68 @@
+package com.github.huifer.simple.shiro.boot.entity;
+
+import java.util.Objects;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shiro_role", schema = "simple-shiro", catalog = "")
+public class ShiroRoleEntity {
+
+  private int id;
+  private String name;
+  private String code;
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Basic
+  @Column(name = "name")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Basic
+  @Column(name = "code")
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ShiroRoleEntity that = (ShiroRoleEntity) o;
+    return id == that.id && Objects.equals(name, that.name) && Objects
+        .equals(code, that.code);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, code);
+  }
+}
---
+++ docs/spring/cs78d15e94-aa04-11ef-a4b6-acde48001122.java
@@ -0,0 +1,37 @@
+package com.huifer.concurrence.disruptor.quick;
+
+import com.lmax.disruptor.RingBuffer;
+
+import java.nio.ByteBuffer;
+
+/**
+ * <p>Title : OrderEventProducer </p>
+ * <p>Description : 订单生产者</p>
+ *
+ * @author huifer
+ * @date 2019-06-04
+ */
+public class OrderEventProducer {
+
+    private RingBuffer<OrderEvent> ringBuffer;
+
+    public OrderEventProducer(RingBuffer<OrderEvent> ringBuffer) {
+        this.ringBuffer = ringBuffer;
+    }
+
+    public void send(ByteBuffer byteBuffer) {
+        //1.从RingBuffer中获取可用序号
+        long sequence = ringBuffer.next();
+        try {
+            //2. 根据sequence 需要找到一个没有数据内容的 Event
+            OrderEvent orderEvent = ringBuffer.get(sequence);
+            //3. 填充数据
+            orderEvent.setValue(byteBuffer.getInt(0));
+        } finally {
+            //4. 提交数据
+            ringBuffer.publish(sequence);
+        }
+
+    }
+
+}
