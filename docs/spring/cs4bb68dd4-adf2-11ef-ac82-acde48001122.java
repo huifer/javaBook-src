fe500ab7199bca8459b75005eaf700a45feb8c48
Zen Huifer <huifer97@163.com>
2024-11-29 09:36:04 +0800

    write a line into test.file

---
+++ docs/spring/cs4bb68dd4-adf2-11ef-ac82-acde48001122.java
@@ -0,0 +1,26 @@
+package com.huifer.security.browser.support;
+
+/**
+ * 描述:
+ *
+ * @author: huifer
+ * @date: 2019-11-17
+ */
+public class Response {
+    private Object object;
+
+    public Response() {
+    }
+
+    public Response(Object object) {
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
