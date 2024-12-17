39295a42dec27561777556adb34c6c3cc6a3da1a
Zen Huifer <huifer97@163.com>
2024-12-17 09:36:19 +0800

    write a line into test.file

---
+++ docs/spring/cs4e74b95a-bc17-11ef-8422-acde48001122.java
@@ -0,0 +1,16 @@
+package com.huifer.proxy.impl;
+
+import com.huifer.proxy.BaseService;
+
+/**
+ * 描述:
+ *
+ * @author huifer
+ * @date 2019-03-02
+ */
+public class BaseServiceImpl implements BaseService {
+    @Override
+    public String doSome() {
+        return "implimplimplimpl";
+    }
+}
