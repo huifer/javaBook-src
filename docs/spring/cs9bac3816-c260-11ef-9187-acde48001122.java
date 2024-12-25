586b87ec50d9e8f4333c140f2dcd0a9908559647
Zen Huifer <huifer97@163.com>
2024-12-25 09:36:06 +0800

    write a line into test.file

---
+++ docs/spring/cs9bac3816-c260-11ef-9187-acde48001122.java
@@ -0,0 +1,74 @@
+package org.huifer.rbac.controller;
+
+
+import javax.validation.Valid;
+
+import org.huifer.rbac.entity.req.IdsReq;
+import org.huifer.rbac.entity.req.PageReq;
+import org.huifer.rbac.entity.req.user.UserAddReq;
+import org.huifer.rbac.entity.req.user.UserBindRoleReq;
+import org.huifer.rbac.entity.req.user.UserEditorReq;
+import org.huifer.rbac.entity.req.user.UserQueryReq;
+import org.huifer.rbac.entity.res.Result;
+import org.huifer.rbac.service.IUserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    final
+    IUserService userService;
+
+    public UserController(@Qualifier("userService") IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/add")
+    public Result add(
+            @RequestBody @Valid UserAddReq req
+    ) {
+        return userService.add(req);
+    }
+
+    @PostMapping(value = "/query" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public Result query(
+            @RequestBody @Valid    UserQueryReq req,
+             @Validated PageReq pageReq
+    ) {
+
+        return userService.query(req, pageReq);
+    }
+
+    @PostMapping("/editor")
+    public Result editor(
+            @RequestBody @Validated UserEditorReq req
+    ) {
+        return userService.editor(req);
+
+    }
+
+    @PostMapping("/delete")
+    public Result delete(
+            @RequestBody @Validated IdsReq req
+    ) {
+        return userService.delete(req);
+
+    }
+
+    @PostMapping("/setting_role")
+    public Result settingRole(
+            @RequestBody @Validated UserBindRoleReq req
+    ) {
+        return userService.settingRole(req);
+    }
+
+}
