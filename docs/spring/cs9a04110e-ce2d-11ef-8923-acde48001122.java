6de43435323b5475fcd5c69bf81dcee39a0b47db
Zen Huifer <huifer97@163.com>
2025-01-09 10:01:15 +0800

    write a line into test.file

---
+++ docs/spring/cs9a04110e-ce2d-11ef-8923-acde48001122.java
@@ -0,0 +1,17 @@
+package com.huifer.design.builder;
+
+/**
+ * 轮子
+ */
+public class Wheel {
+
+  private Integer size;
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+}
