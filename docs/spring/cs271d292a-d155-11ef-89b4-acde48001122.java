ab70247e4306f0f2a8332f511503805e6191a277
Zen Huifer <huifer97@163.com>
2025-01-13 10:21:54 +0800

    write a line into test.file

---
+++ docs/spring/cs271d292a-d155-11ef-89b4-acde48001122.java
@@ -0,0 +1,27 @@
+package com.github.huifer.shiro;
+
+import static org.junit.Assert.*;
+import java.nio.charset.StandardCharsets;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+public class CustomerRealmTest {
+
+  @Test
+  public void testCustomerRealm(){
+    DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+    defaultSecurityManager.setRealm(new CustomerRealm());
+    SecurityUtils.setSecurityManager(defaultSecurityManager);
+    Subject subject = SecurityUtils.getSubject();
+    UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("admin", "admin2123");
+    subject.login(usernamePasswordToken);
+
+  }
+
+
+
+}
