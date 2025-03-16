2b00effa1923f3566b4635c8a385cd58d7693af5
ZenHuiFer <huifer97@163.com>
2025-03-16 09:00:02 +0800

    test

---
+++ docs/spring/97f3056e-d513-4d6d-873a-bbd5df7d9672.java
@@ -0,0 +1,14 @@
+package com.github.huifer.full.shiro.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.huifer.full.shiro.entity.ShiroPostBindRole;
+
+/**
+ * 岗位角色关系表(ShiroPostBindRole)表数据库访问层
+ *
+ * @author huifer
+ * @since 2021-04-18 10:08:20
+ */
+public interface ShiroPostBindRoleDao extends BaseMapper<ShiroPostBindRole> {
+
+}
