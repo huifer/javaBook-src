608e840249b2b7764c6bb35e211f28ea67cfdc19
ZenHuiFer <huifer97@163.com>
2025-03-26 09:00:01 +0800

    test

---
+++ docs/spring/eb4eff1f-3e7f-44f0-9971-bac05f2be9ba.java
@@ -0,0 +1,14 @@
+package com.huifer.note.auth2.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Auth2ServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Auth2ServiceApp.class, args);
+    }
+
+
+}
