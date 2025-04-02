1ceea37db370a2140308b48a2c25e3fa2d66063d
ZenHuiFer <huifer97@163.com>
2025-04-02 09:00:01 +0800

    test

---
+++ docs/spring/40147241-79de-4b62-97b8-3479fb4009b7.java
@@ -0,0 +1,20 @@
+package com.huifer.security.properties;
+
+/**
+ * 描述:
+ *
+ * @author: huifer
+ * @date: 2019-11-17
+ */
+public enum LoginType {
+
+    /**
+     * json
+     */
+    JSON,
+    /**
+     * 跳转
+     */
+    REDIRECT,
+    ;
+}
