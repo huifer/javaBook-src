d8329992064bb18ab536d1e637d645e9b29917b8
ZenHuiFer <huifer97@163.com>
2025-04-08 09:00:01 +0800

    test

---
+++ docs/spring/739736ef-baa4-4202-ae96-1e46c1df9bdc.java
@@ -0,0 +1,35 @@
+package com.huifer.mybatis.handler;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestTypeHandler implements TypeHandler {
+    @Override
+    public void setParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+
+    }
+
+    /**
+     * @param rs
+     * @param columnName Colunm name, when configuration <code>useColumnLabel</code> is <code>false</code>
+     */
+    @Override
+    public Object getResult(ResultSet rs, String columnName) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Object getResult(ResultSet rs, int columnIndex) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Object getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return null;
+    }
+}
