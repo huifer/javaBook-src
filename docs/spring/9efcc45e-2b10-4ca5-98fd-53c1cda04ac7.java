1f687ff9126bf8618d2a4c065cee93299cab5c3d
ZenHuiFer <huifer97@163.com>
2025-05-05 09:00:01 +0800

    test

---
+++ docs/spring/9efcc45e-2b10-4ca5-98fd-53c1cda04ac7.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.executor;
+
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.cache.TransactionalCacheManager;
+import org.apache.ibatis.cursor.Cursor;
+import org.apache.ibatis.mapping.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.transaction.Transaction;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 缓存执行器
+ *
+ * @author Clinton Begin
+ * @author Eduardo Macarron
+ */
+public class CachingExecutor implements Executor {
+
+    /**
+     * 执行器
+     */
+    private final Executor delegate;
+    private final TransactionalCacheManager tcm = new TransactionalCacheManager();
+
+    public CachingExecutor(Executor delegate) {
+        this.delegate = delegate;
+        delegate.setExecutorWrapper(this);
+    }
+
+    @Override
+    public Transaction getTransaction() {
+        return delegate.getTransaction();
+    }
+
+    @Override
+    public void close(boolean forceRollback) {
+        try {
+            //issues #499, #524 and #573
+            if (forceRollback) {
+                tcm.rollback();
+            } else {
+                tcm.commit();
+            }
+        } finally {
+            delegate.close(forceRollback);
+        }
+    }
+
+    @Override
+    public boolean isClosed() {
+        return delegate.isClosed();
+    }
+
+    @Override
+    public int update(MappedStatement ms, Object parameterObject) throws SQLException {
+        flushCacheIfRequired(ms);
+        return delegate.update(ms, parameterObject);
+    }
+
+    @Override
+    public <E> List<E> query(MappedStatement ms, Object parameterObject, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException {
+        BoundSql boundSql = ms.getBoundSql(parameterObject);
+        CacheKey key = createCacheKey(ms, parameterObject, rowBounds, boundSql);
+        return query(ms, parameterObject, rowBounds, resultHandler, key, boundSql);
+    }
+
+    @Override
+    public <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException {
+        flushCacheIfRequired(ms);
+        return delegate.queryCursor(ms, parameter, rowBounds);
+    }
+
+    @Override
+    public <E> List<E> query(MappedStatement ms, Object parameterObject, RowBounds rowBounds, ResultHandler resultHandler, CacheKey key, BoundSql boundSql)
+            throws SQLException {
+        // 标签<cache>
+        Cache cache = ms.getCache();
+        if (cache != null) {
+            flushCacheIfRequired(ms);
+            if (ms.isUseCache() && resultHandler == null) {
+                // 判断是否使用缓存 和 结果处理器
+                ensureNoOutParams(ms, boundSql);
+                @SuppressWarnings("unchecked")
+                List<E> list = (List<E>) tcm.getObject(cache, key);
+                if (list == null) {
+                    // 二级缓存是否有数据,查询流程:2级缓存\1级缓存\数据库
+                    list = delegate.query(ms, parameterObject, rowBounds, resultHandler, key, boundSql);
+                    tcm.putObject(cache, key, list); // issue #578 and #116
+                }
+                return list;
+            }
+        }
+        // 数据库查询
+        return delegate.query(ms, parameterObject, rowBounds, resultHandler, key, boundSql);
+    }
+
+    @Override
+    public List<BatchResult> flushStatements() throws SQLException {
+        return delegate.flushStatements();
+    }
+
+    @Override
+    public void commit(boolean required) throws SQLException {
+        // 执行提交方法
+        delegate.commit(required);
+        // 缓存事物提交
+        tcm.commit();
+    }
+
+    @Override
+    public void rollback(boolean required) throws SQLException {
+        try {
+            // 事物回滚
+            delegate.rollback(required);
+        } finally {
+            if (required) {
+                tcm.rollback();
+            }
+        }
+    }
+
+    private void ensureNoOutParams(MappedStatement ms, BoundSql boundSql) {
+        if (ms.getStatementType() == StatementType.CALLABLE) {
+            for (ParameterMapping parameterMapping : boundSql.getParameterMappings()) {
+                if (parameterMapping.getMode() != ParameterMode.IN) {
+                    throw new ExecutorException("Caching stored procedures with OUT params is not supported.  Please configure useCache=false in " + ms.getId() + " statement.");
+                }
+            }
+        }
+    }
+
+    @Override
+    public CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql) {
+        return delegate.createCacheKey(ms, parameterObject, rowBounds, boundSql);
+    }
+
+    @Override
+    public boolean isCached(MappedStatement ms, CacheKey key) {
+        return delegate.isCached(ms, key);
+    }
+
+    @Override
+    public void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType) {
+        delegate.deferLoad(ms, resultObject, property, key, targetType);
+    }
+
+    @Override
+    public void clearLocalCache() {
+        delegate.clearLocalCache();
+    }
+
+    private void flushCacheIfRequired(MappedStatement ms) {
+        Cache cache = ms.getCache();
+        if (cache != null && ms.isFlushCacheRequired()) {
+            tcm.clear(cache);
+        }
+    }
+
+    @Override
+    public void setExecutorWrapper(Executor executor) {
+        throw new UnsupportedOperationException("This method should not be called");
+    }
+
+}
