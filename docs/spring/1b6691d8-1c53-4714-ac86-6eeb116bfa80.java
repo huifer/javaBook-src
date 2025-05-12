cf22c4f760ab92fd26a7c59f6c7bd8f1efa17df9
ZenHuiFer <huifer97@163.com>
2025-05-12 09:00:01 +0800

    test

---
+++ docs/spring/1b6691d8-1c53-4714-ac86-6eeb116bfa80.java
@@ -0,0 +1,30 @@
+package com.huifer.idgen.my.service.provider;
+
+/**
+ * @author: wang
+ * @description:
+ */
+public class PropertyMachineIdsProvider implements MachineIdsProvider {
+
+
+    private long[] machineIds;
+
+
+    private int currentIndex;
+
+
+    @Override
+    public long getMachineId() {
+        return machineIds[currentIndex++ % machineIds.length];
+    }
+
+    public long[] getMachineIds() {
+        return machineIds;
+    }
+
+    public void setMachineIds(long[] machineIds) {
+        this.machineIds = machineIds;
+    }
+
+
+}
