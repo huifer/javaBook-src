4c84284fc19038ca40503e0e0f797a87c265be47
ZenHuiFer <huifer97@163.com>
2025-05-19 09:00:01 +0800

    test

---
+++ docs/spring/1dae7865-1020-46b6-969b-025ff1b173b0.java
@@ -0,0 +1,39 @@
+package com.huifer.concurrence.ch2;
+
+/**
+ * <p>Title : DataRace </p>
+ * <p>Description : 数据竞争</p>
+ *
+ * @author huifer
+ * @date 2019-03-26
+ */
+public class DataRace {
+
+    // 输出运算流程：从0开始累加1
+    private static Integer calc = 0;
+
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            for (int i = 0; i < 5; i++) {
+                addOneSynchronized();
+            }
+        };
+        Thread t1 = new Thread(r, "线程01");
+        Thread t2 = new Thread(r, "线程02");
+        t1.start();
+        t2.start();
+    }
+
+    private synchronized static void addOneSynchronized() {
+        int now = calc;
+        calc++;
+        System.out.println(Thread.currentThread().getName() + " - 操作前 " + now + " 操作后 " + calc);
+    }
+
+    private static void addOne() {
+        int now = calc;
+        calc++;
+        System.out.println(Thread.currentThread().getName() + " - 操作前 " + now + " 操作后 " + calc);
+    }
+
+}
