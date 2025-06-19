e8509324f3340ad9cdabc1bfbc955af4e966e765
ZenHuiFer <huifer97@163.com>
2025-06-19 09:00:01 +0800

    test

---
+++ docs/spring/608934f7-ca81-4ce0-8318-f472c4e23266.java
@@ -0,0 +1,16 @@
+package com.huifer.jdk.abs;
+
+/**
+ * <p>Title : Absc </p>
+ * <p>Description : </p>
+ *
+ * @author huifer
+ * @date 2019-05-24
+ */
+public abstract class Absc {
+
+    void hello() {
+        System.out.println("hello");
+    }
+
+}
