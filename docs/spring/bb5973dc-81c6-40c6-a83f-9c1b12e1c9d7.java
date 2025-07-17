38db3f637100b42ca3dcb95b06e8f36ea6fd6336
ZenHuiFer <huifer97@163.com>
2025-07-17 09:00:01 +0800

    test

---
+++ docs/spring/bb5973dc-81c6-40c6-a83f-9c1b12e1c9d7.java
@@ -0,0 +1,20 @@
+package com.huifer.fzjh.exmple;
+
+import com.huifer.fzjh.service.LoadBalanceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Slf4j
+public class SpringLoadBalance {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-loadBalance.xml");
+        LoadBalanceService loadBalanceFactoryBean = (LoadBalanceService) context.getBean("loadBalanceFactoryBean");
+        for (int i = 0; i < 15; i++) {
+
+            String loadBalanceId = loadBalanceFactoryBean.loadBalance();
+            log.info(loadBalanceId);
+        }
+
+    }
+}
